636979c8cf21fba8647e81b03e6d7e5b609076c9
Jeffrey Lakin <neocount@gmail.com>
2021-04-13 13:49:19 -0400

    commit tree bacee9db5ed042f44ef476493a48b36eff103dd9

--- index.js
+++ index.js
@@ -9,5 +9,33 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(s) { 
+  // The greet function should take one argument,
+  // a String which specifies the 24-hour time in the format HH:MM.
+  const words = s.split(':');
+  const hour24 = parseInt(words[0], 10);
+  if(hour24 < 12) { 
+    // "Good Morning"
+    return "Good Morning";
+  }
+  else if(hour24 < 17) { 
+    //"Good Afternoon"
+    return "Good Afternoon";
+  }
+  else { 
+    // "Good Evening"
+    return "Good Evening";
+  }
+}
+
+function displayMessage(s) {
+
+//   The displayMessage function should take one argument, a String.
+//   When the function runs it should update the text inside the #greeting 
+//   node with the content of the argument.
+//   It does not return anything.
+  document.getElementById("greeting").innerText = s;
+
+}
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
